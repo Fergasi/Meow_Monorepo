f001b28de34f4bc802dac77d947e38aad24c1472
Fergasi <98353142+Fergasi@users.noreply.github.com>
2022-11-02 00:37:13 -0400

    deploy

--- backend/routes/messageRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { allMessages, sendMessage } = require("../services/MessageServices");
-
-const messageRouter = express.Router();
-
-messageRouter.get("/:chatId", allMessages);
-messageRouter.post("/", sendMessage);
-
-module.exports = messageRouter;
